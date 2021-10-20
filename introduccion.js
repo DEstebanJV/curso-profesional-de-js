136902b29d5fa641d05782e50c42ff6dc27a2870
DEstebanJV <davidjv312@gmail.com>
2021-10-20 14:55:41 -0500

    introduccion al Curso

---
+++ introduccion.js
@@ -0,0 +1,26 @@
+/*  Que significar ser un profesional de javascript */
+
+// ¿ que forma a un profesional ?
+
+/* 
+- conocimiento del lenguaje 
+- versado en codigo
+- conocimiento de los entornos de programacion
+- herramientas
+-mejores practicas
+- etica / profesionalismo
+-experiencia
+*/
+
+/*  ASPECTOS QUE DESTACAN A UN PROFESIONAL 
+
+ Mejores practicas 
+ Escribir codigo optimizado que logre el mejor resultado
+*/
+
+/* Conocer el entorno de programacion
+Dominar las sintaxis  y las herramientas te ayudara a reducir la cantidad de errores
+*/
+
+
+
